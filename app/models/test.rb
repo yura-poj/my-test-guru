a84248326112d73955b69dd52620cfcc4183e6b2
yura-poj <85958170+yura-poj@users.noreply.github.com>
2021-07-13 18:23:40 +0700

    add def to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.tests_with_category(category)
+    category_id = Category.find_by(title: category).id
+    Test.where(category_id: category_id).order(id: :desc)
+  end
 end
