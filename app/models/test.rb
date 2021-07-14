a6c18c67bad0d7c8701a587407612668a4f71c7c
yura-poj <85958170+yura-poj@users.noreply.github.com>
2021-07-14 19:24:27 +0700

    add tests_user

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,9 @@ class Test < ApplicationRecord
   has_one :category
   has_many :questions
 
+  has_many :tests_users
+  has_many :users , through: :tests_users
+
   def self.tests_with_category(category)
     category_id = Category.find_by(title: category).id
     Test.where(category_id: category_id).order(id: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :answers
+  has_one :test
+  has_many :tests_users
+  has_many :tests , through: :tests_users
   def tests_by_level(level)
     Test.where(id: Result.where(id: id).test_id).where(level: level)
   end
