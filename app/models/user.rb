fbc35fd601ff93b216c094bae58ee7d64ecefc91
yura-poj <85958170+yura-poj@users.noreply.github.com>
2021-07-16 15:13:49 +0700

    change def tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests , through: :tests_users
   def tests_by_level(level)
-    Test.where(id: Result.where(id: id).test_id).where(level: level)
+    self.tests.by_level(level)
   end
 end
