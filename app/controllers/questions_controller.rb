f7596c9697c41a83cc03d42488e6dcc6c5179b14
yura-poj <85958170+yura-poj@users.noreply.github.com>
2021-07-22 12:32:37 +0700

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,45 @@
 class QuestionsController < ApplicationController
 
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show delete]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def show
-    test_id = params[:test_id]
     id = params[:id]
-    render plain: Test.find(test_id).questions.find(id).inspect
+    render plain: @question.inspect
   end
 
   def index
-    render plain: Test.find(params[:test_id]).questions.inspect
+    render plain: @test.questions.inspect
+  end
+
+  def new;end
+
+  def create
+    question = Question.create(test_params)
+
+    render plain: question.inspect
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[id])
+  end
+
+  def test_params
+    params.require(:question).permit(:body, :test_id)
   end
 end
