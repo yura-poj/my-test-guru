5f9e56ba11b7ad5d883ff3d495548fbb0f99df97
yura-poj <yurap6002@gmail.com>
2022-02-10 13:50:07 +0700

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,10 +71,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'secret-sands-86445.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:                'yurap6002@gmail.com',
+    address:                'smtp.gmail.com',
     port:                   587,
     user_name:              ENV['SMTP_USERNAME'],
-    password:               'yura20066002',
+    password:               ENV['SMTP_PASSWORD'],
     authentication:         'plain',
     enable_starttls_auto:   true }
 
