654c19505860a6c08116ac176fa5cd0849a49e95
yura-poj <yurap6002@gmail.com>
2022-02-10 13:11:53 +0700

    configs action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "my_test_guru_production"
 
-  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'secret-sands-86445.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   587,
+    user_name:              ENV['SMTP_USERNAME'],
+    password:               ENV['SMTP_PASSWORD'],
+    authentication:         'plain',
+    enable_starttls_auto:   true }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -84,13 +94,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.smtp_settings = {
-    address:                'smtp.gmail.com',
-    port:                   587,
-    user_name:              ENV['SMTP_USERNAME'],
-    password:               ENV['SMTP_PASSWORD'],
-    authentication:         'plain',
-    enable_starttls_auto:   true }
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
